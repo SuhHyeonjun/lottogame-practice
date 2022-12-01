2cfa013f1b9b85f1b86b5c5c4c7dc912eae02c38
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-12-01 17:00:45 +0900

    feat : 구입 금액 예외 메서드 추가

---
+++ src/main/java/lotto/validator/InputPriceValidator.java
@@ -0,0 +1,20 @@
+package lotto.validator;
+
+import lotto.ErrorMessage;
+
+public class InputPriceValidator {
+    private static final int DIVIDE = 1000;
+    private static final int ZERO = 0;
+
+    public static void validatePurchasePrice(int purchasePrice) {
+        if (purchasePrice % DIVIDE != ZERO) {
+            throw new IllegalArgumentException(ErrorMessage.ERROR_INPUT_PURCHASE.getErrorMessage());
+        }
+    }
+
+    public static void validateZero(int purchasePrice) {
+        if (purchasePrice == ZERO) {
+            throw new IllegalArgumentException(ErrorMessage.ERROR_INPUT_ZERO.getErrorMessage());
+        }
+    }
+}
