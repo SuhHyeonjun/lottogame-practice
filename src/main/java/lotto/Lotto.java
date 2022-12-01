bad1d7ee3fc3971259e7030a5f05dea5993a33d7
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-12-01 15:18:15 +0900

    feat : 1부터 45범위의 수를 가지는 6개의 번호 생성

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,8 +1,13 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 
 public class Lotto {
+    private static final int START_RANGE = 1;
+    private static final int END_RANGE = 45;
+    private static final int LOTTO_COUNT = 6;
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -10,11 +15,19 @@ public class Lotto {
         this.numbers = numbers;
     }
 
+    public static List<Integer> makeLotto() {
+        List<Integer> lotto = Randoms.pickUniqueNumbersInRange(START_RANGE, END_RANGE, LOTTO_COUNT);
+        return lotto;
+    }
+
+    @Override
+    public String toString() {
+        return numbers.toString();
+    }
+
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
     }
-
-    // TODO: 추가 기능 구현
 }
