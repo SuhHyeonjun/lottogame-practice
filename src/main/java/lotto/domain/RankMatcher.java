6486891e398540b0f801e294ccaaba75178b744b
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-12-02 17:08:34 +0900

    feat : 로또 번호와 당첨 번호가 일치하는 개수 구하는 기능

--- src/main/java/lotto/domain/RankMatcher.java
+++ src/main/java/lotto/domain/RankMatcher.java
@@ -6,11 +6,18 @@ public class RankMatcher {
 
     public static int countMatchNumbers(List<List<Integer>> lottos, List<Integer> winningNumbers) {
         int matchCount = 0;
+        int maxCount = 0;
         for (List<Integer> lotto : lottos) {
-            if (lotto.contains()
-
+            for (Integer match : winningNumbers) {
+                if (lotto.contains(match)) {
+                    matchCount++;
+                }
+            }
+            maxCount = Math.max(matchCount, maxCount);
+            System.out.println(maxCount);
+            matchCount = 0;
         }
-
+        return maxCount;
     }
 
     public static boolean matchBonus(List<List<Integer>> lottos, int bonusNumber) {
