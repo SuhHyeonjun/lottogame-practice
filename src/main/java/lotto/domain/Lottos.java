411d237c69e15fc679647d3d9bb9ceb89aa2de92
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-12-02 12:43:24 +0900

    feat : 구매한 개수 만큼 로또 출력

--- src/main/java/lotto/domain/Lottos.java
+++ src/main/java/lotto/domain/Lottos.java
@@ -2,14 +2,19 @@ package lotto.domain;
 
 import static lotto.domain.Lotto.makeLotto;
 
+import java.util.List;
+import lotto.view.Message;
 import lotto.view.OutputView;
 
 public class Lottos {
     private static final int DIVIDE = 1000;
 
     public static void makeLottos(int purchasePrice) {
-        for (int i = 0; i < purchasePrice/DIVIDE; i++) {
-            OutputView.printLottos(new Lotto(makeLotto()));
+        int lottoAmount = purchasePrice / DIVIDE;
+        System.out.println(lottoAmount + Message.PURCHASE_AMOUNT.getMessage());
+        for (int i = 0; i < lottoAmount; i++) {
+            List<Integer> numbers = makeLotto();
+            OutputView.printLottos(new Lotto(numbers));
         }
     }
 }
