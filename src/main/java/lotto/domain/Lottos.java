451ee695b019e68105bddf8f055f54ee1f015e94
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-12-02 16:51:55 +0900

    refactor : 2차원 List를 반환하도록 변경

--- src/main/java/lotto/domain/Lottos.java
+++ src/main/java/lotto/domain/Lottos.java
@@ -2,15 +2,19 @@ package lotto.domain;
 
 import static lotto.domain.Lotto.makeLotto;
 
+import java.util.ArrayList;
 import java.util.List;
 import lotto.view.OutputView;
 
 public class Lottos {
 
-    public static void makeLottos(int lottoAmount) {
+    public static List<List<Integer>> makeLottos(int lottoAmount) {
+        List<List<Integer>> lottos = new ArrayList<>();
         for (int i = 0; i < lottoAmount; i++) {
             List<Integer> numbers = makeLotto();
+            lottos.add(numbers);
             OutputView.printLottos(new Lotto(numbers));
         }
+        return lottos;
     }
 }
