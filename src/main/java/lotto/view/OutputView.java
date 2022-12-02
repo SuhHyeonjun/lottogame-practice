5064706b946fa3e904bac8a0bac83df803e26604
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-12-02 12:47:51 +0900

    feat : 구매한 로또 개수 출력

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -7,4 +7,8 @@ public class OutputView {
     public static void printLottos(Lotto lotto) {
         System.out.println(lotto);
     }
+
+    public static void printAmount(int lottoAmount) {
+        System.out.println(lottoAmount + Message.PURCHASE_AMOUNT.getMessage());
+    }
 }
