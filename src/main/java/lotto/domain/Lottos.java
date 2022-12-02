a322eeeb06e7636266c98873e679be3f136a2f2c
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-12-02 12:48:20 +0900

    refactor : 출력 로직 분리

--- src/main/java/lotto/domain/Lottos.java
+++ src/main/java/lotto/domain/Lottos.java
@@ -3,15 +3,12 @@ package lotto.domain;
 import static lotto.domain.Lotto.makeLotto;
 
 import java.util.List;
-import lotto.view.Message;
 import lotto.view.OutputView;
 
 public class Lottos {
-    private static final int DIVIDE = 1000;
 
-    public static void makeLottos(int purchasePrice) {
-        int lottoAmount = purchasePrice / DIVIDE;
-        System.out.println(lottoAmount + Message.PURCHASE_AMOUNT.getMessage());
+
+    public static void makeLottos(int lottoAmount) {
         for (int i = 0; i < lottoAmount; i++) {
             List<Integer> numbers = makeLotto();
             OutputView.printLottos(new Lotto(numbers));
