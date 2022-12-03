f5ddf23a21b2ee7e4534c9953bf490836af067b0
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-12-03 11:49:53 +0900

    feat : 가장 많은 번호가 일치하는 로또 구하기

--- src/main/java/lotto/domain/RankMatcher.java
+++ src/main/java/lotto/domain/RankMatcher.java
@@ -1,32 +1,49 @@
 package lotto.domain;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class RankMatcher {
+    private static int matchCount = 0;
 
-    public static int countMatchNumbers(List<List<Integer>> lottos, List<Integer> winningNumbers) {
-        int matchCount = 0;
-        int maxCount = 0;
+    public static List<Integer> getMostMatchLotto(List<List<Integer>> lottos, List<Integer> winningNumbers) {
+        int count = 0;
+        Map<List<Integer>, Integer> map = new HashMap<>();
         for (List<Integer> lotto : lottos) {
             for (Integer match : winningNumbers) {
                 if (lotto.contains(match)) {
-                    matchCount++;
+                    count++;
+                    map.put(lotto, count);
                 }
             }
-            maxCount = Math.max(matchCount, maxCount);
-            System.out.println(maxCount);
-            matchCount = 0;
+            System.out.println(map);
+            count = 0;
         }
-        return maxCount;
+        count = Collections.max(map.values());
+        matchCount = count;
+        List<Integer> mostMatchLotto = new ArrayList<>();
+        for (Map.Entry<List<Integer>, Integer> entry : map.entrySet()) {
+            if (entry.getValue().equals(matchCount)) {
+                mostMatchLotto = entry.getKey();
+                break;
+            }
+        }
+        System.out.println(mostMatchLotto);
+        return mostMatchLotto;
     }
 
-    public static boolean matchBonus(List<List<Integer>> lottos, int bonusNumber) {
-        for (List<Integer> lotto : lottos) {
-            if (lotto.contains(bonusNumber)) {
-                return true;
-            }
-            return false;
+    public static boolean matchBonus(List<Integer> mostMatchLotto, int bonusNumber) {
+        if (mostMatchLotto.contains(bonusNumber)) {
+            return true;
         }
         return false;
+    }
+
+    public static int getMatchCount() {
+        return matchCount;
     }
 }
