7f1a7da56370a6aae5708ab0b088d0e7adfa0e67
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-12-05 19:15:04 +0900

    feat : 로또 당첨 내역 출력 기능 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,6 +1,9 @@
 package lotto.view;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
 import lotto.domain.Lotto;
+import lotto.domain.LottoRank;
 
 public class OutputView {
 
@@ -10,5 +13,14 @@ public class OutputView {
 
     public static void printAmount(int lottoAmount) {
         System.out.println(lottoAmount + Message.PURCHASE_AMOUNT.getMessage());
+    }
+
+    public static void printWinningLists(Map<LottoRank, Integer> result) {
+        for (LottoRank rank : LottoRank.values()) {
+            if (rank.getMoney() == 0) {
+                continue;
+            }
+            System.out.println(rank.getRank() + result.get(rank) + "개");
+        }
     }
 }
