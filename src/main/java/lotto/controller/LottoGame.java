2609a830c0eca519f1069fb0131836a9b6b80fdb
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-12-05 20:16:15 +0900

    feat : 예외 처리 추가

--- src/main/java/lotto/controller/LottoGame.java
+++ src/main/java/lotto/controller/LottoGame.java
@@ -16,22 +16,26 @@ public class LottoGame {
     private static final int DIVIDE = 1000;
 
     public void start() {
-        int purchasePrice = InputView.inputPurchasePrice();
-        int lottoAmount = purchasePrice / DIVIDE;
-        System.out.println();
-        OutputView.printAmount(lottoAmount);
-        List<List<Integer>> lottos = Lottos.makeLottos(lottoAmount);
-        System.out.println(lottos);
-        List<Integer> winningNumbers = InputView.inputWinningNumber();
-        int bonusNumber = InputView.inputBonusNumber();
-        checkOverlap(winningNumbers, bonusNumber);
-        List<Integer> mostMatchLotto = RankMatcher.getMostMatchLotto(lottos, winningNumbers);
-        int matchCount = RankMatcher.getMatchCount();
-        System.out.println(matchCount);
-        boolean bonus = RankMatcher.matchBonus(mostMatchLotto, bonusNumber);
-        System.out.println(bonus);
-        Map<LottoRank, Integer> result = setMatchRank(matchCount, bonus);
-        OutputView.printWinningLists(result);
+        try {
+            int purchasePrice = InputView.inputPurchasePrice();
+            int lottoAmount = purchasePrice / DIVIDE;
+            System.out.println();
+            OutputView.printAmount(lottoAmount);
+            List<List<Integer>> lottos = Lottos.makeLottos(lottoAmount);
+            System.out.println(lottos);
+            List<Integer> winningNumbers = InputView.inputWinningNumber();
+            int bonusNumber = InputView.inputBonusNumber();
+            checkOverlap(winningNumbers, bonusNumber);
+            List<Integer> mostMatchLotto = RankMatcher.getMostMatchLotto(lottos, winningNumbers);
+            int matchCount = RankMatcher.getMatchCount();
+            System.out.println(matchCount);
+            boolean bonus = RankMatcher.matchBonus(mostMatchLotto, bonusNumber);
+            System.out.println(bonus);
+            Map<LottoRank, Integer> result = setMatchRank(matchCount, bonus);
+            OutputView.printWinningLists(result);
+        } catch (Exception error) {
+            System.out.println(error.getMessage());
+        }
     }
 
     public static Map<LottoRank, Integer> setMatchRank(int matchCount, boolean bonus) {
